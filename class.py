54a68e7218e8cef4422cada45dd24d175880438b
meera kumari <24kq1a0519@pace.ac.in>
2025-09-16 22:17:51 +0530

    Added my 1st Python assignment

---
+++ class.py
@@ -0,0 +1,22 @@
+class cse:
+    name=""
+    rno=0
+
+c1=cse()
+c1.name="meera"
+c1.rno="519"
+
+c2=cse()
+c2.name="panda"
+c2.rno="18"
+
+c3=cse()
+c3.name="magic"
+c3.rno="509"
+
+print(c1.name)
+print(c1.rno)
+print(c2.name)
+print(c2.rno)
+print(c3.name)
+print(c3.rno)
